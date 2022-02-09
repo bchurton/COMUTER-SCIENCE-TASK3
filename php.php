9e096048be13a95e6caa21b17c12a48afa70594b
Benjamin <ben@churton.uk>
2022-02-09 09:52:49 +0000

    Create php.php

---
+++ php.php
@@ -0,0 +1,12 @@
+<?php
+ 
+$age = (int)readline('What is your age? ');
+ 
+if ($age < 15) {
+  echo "Sorry, you cannot watch this film";
+}
+else {
+  echo "You are old enough to watch this film!";
+}
+
+?>
